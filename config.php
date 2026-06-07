bcdcbb90d8ad8f6364712e296d0d6ba7c7a9fec9
Fujita <manabu.fjt@gmail.com>
2026-06-07 13:37:01 +0900

    環境変数（本番、開発）を整理

--- config.php
+++ config.php
@@ -10,9 +10,14 @@ class Config {
     }
 
     public static function get($s) {
+
+        // 環境によってファイルを切り替える
+        $env = getenv('APP_ENV') ?: 'dev';
+        $file = 'common_' . $env . '.php';
+
         $values = preg_split('/\./', $s, -1, PREG_SPLIT_NO_EMPTY);
         $key = array_pop($values);
-        $file = 'common.php';
+
         $path = (!empty($values)) ? implode(DIRECTORY_SEPARATOR, $values) .
             DIRECTORY_SEPARATOR : '';
         $base_dir = self::getConfigDirectory() . DIRECTORY_SEPARATOR;
